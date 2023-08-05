72a671b2d2573baed36835e74a30f0183e536a9d
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-08-05 19:07:20 +0530

    background

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,30 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
-import Row from "./Row";
-import { letters1, letters2, letters3, numbers, colours } from "../letters";
-
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setString,
-  deleteString,
-  setNewLine,
-} from "../redux/string/stringActions";
-import { setCapital } from "../redux/capital/capitalActions";
+import React, { useState } from "react";
 import TypeWriterContainer from "./TypeWriterContainer";
 
 const Container = () => {
-  const fullString = useSelector((state) => state.fullString.fullString);
-  const colour = useSelector((state) => state.colour.colour);
+  const [orientation, setOrientation] = useState(window.orientation);
 
-  const [colourOptions, setColourOptions] = useState(false);
+  console.log(orientation);
 
-  const textArea = useRef();
-
+  console.log(window.orientation);
   return (
-    // <div className="container 2xl:pt-28 xl:pt-28 lg:pt-28 md:pt-2 w-full">
     <div className="complete-container w-screen h-screen 2xl:pt-28 xl:pt-28 lg:pt-28 md:pt-16">
       <TypeWriterContainer />
-      {/* <h1>HELLo</h1> */}
-      {/* <ModernContainer /> */}
     </div>
   );
 };
